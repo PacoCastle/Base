abdbaa5c0948f867d6d7e4b8ff7a5a7aae3bcbf8
Miguel.Garcia <miguel.garcia@hotelconnections.com>
2020-07-19 10:23:35 -0500

    Swagger integration with Auth

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -30,6 +30,7 @@ using DatingApp.Core;
 using DatingApp.Core.Services;
 using DatingApp.Data;
 using DatingApp.Services;
+using Microsoft.OpenApi.Models;
 
 namespace DatingApp.API
 {
@@ -133,6 +134,37 @@ namespace DatingApp.API
             services.AddScoped<IMachinePartsAttemptsService, MachinePartsAttemptsService>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "My API",
+                    Version = "v1"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -184,6 +216,16 @@ namespace DatingApp.API
                 endpoints.MapFallbackToController("Index", "Fallback");
                 endpoints.MapHub<SignalHub>("/signalHub");
  
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
