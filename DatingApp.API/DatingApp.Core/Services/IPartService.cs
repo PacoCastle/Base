104c8dae9db4f99624a0024f86e707896707feaf
Paco Castillo <castillo-fj@hotmail.com>
2020-07-26 22:05:30 -0500

    Se migra Part a la nueva estructura

--- DatingApp.API/DatingApp.Core/Services/IPartService.cs
+++ DatingApp.API/DatingApp.Core/Services/IPartService.cs
@@ -7,10 +7,10 @@ namespace DatingApp.Core.Services
 {
     public interface IPartService
     {
-        Task<IEnumerable<PartModel>> GetParts();
-        Task<PartModel> GetPartById(int id);
-        Task<PartModel> CreatePart(PartModel partModel);
-        Task  UpdatePart(PartModel partModel , PartModel partUpdateModel);
-        Task<PartModel> GetPartByName(string name);
+        Task<BaseResponse<IEnumerable<PartModel>>> GetParts();
+        Task<BaseResponse<PartModel>> GetPartById(int id);
+        Task<BaseResponse<PartModel>>  CreatePart(PartModel partModel);
+        Task<BaseResponse<PartModel>>  UpdatePart(PartModel partModel , PartModel partUpdateModel);
+        Task<BaseResponse<PartModel>>  GetPartByName(string name);
     }
 }
